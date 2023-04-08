9b62f32404951f04804af273af9ae643c7645762
Ray <raydatray@gmail.com>
2023-04-08 15:02:02 -0400

    Fixed flatten

--- src/assignment3/Block.java
+++ src/assignment3/Block.java
@@ -66,6 +66,7 @@ public class Block {
 		}
 	}
 
+	
 	/*
 	  * Updates size and position for the block and all of its sub-blocks, while
 	  * ensuring consistency between the attributes and the relationship of the 
@@ -119,16 +120,16 @@ public class Block {
 	}
 
 	/*
-  	* Returns a List of blocks to be drawn to get a graphical representation of this block.
-  	* 
-  	* This includes, for each undivided Block:
-  	* - one BlockToDraw in the color of the block
-  	* - another one in the FRAME_COLOR and stroke thickness 3
-  	* 
-  	* Note that a stroke thickness equal to 0 indicates that the block should be filled with its color.
-  	*  
-  	* The order in which the blocks to draw appear in the list does NOT matter.
-  	*/
+	 * Returns a List of blocks to be drawn to get a graphical representation of this block.
+	 *
+	 * This includes, for each undivided Block:
+	 * - one BlockToDraw in the color of the block
+	 * - another one in the FRAME_COLOR and stroke thickness 3
+	 *
+	 * Note that a stroke thickness equal to 0 indicates that the block should be filled with its color.
+	 *
+	 * The order in which the blocks to draw appear in the list does NOT matter.
+	 */
 	public ArrayList<BlockToDraw> getBlocksToDraw() {
 		ArrayList<BlockToDraw> board = new ArrayList<BlockToDraw>();
 
@@ -149,20 +150,20 @@ public class Block {
 	public BlockToDraw getHighlightedFrame() {
 		return new BlockToDraw(GameColors.HIGHLIGHT_COLOR, this.xCoord, this.yCoord, this.size, 5);
 	}
- 
+
 	/*
 	 * Return the Block within this Block that includes the given location
-	 * and is at the given level. If the level specified is lower than 
-	 * the lowest block at the specified location, then return the block 
+	 * and is at the given level. If the level specified is lower than
+	 * the lowest block at the specified location, then return the block
 	 * at the location with the closest level value.
-	 * 
-	 * The location is specified by its (x, y) coordinates. The lvl indicates 
+	 *
+	 * The location is specified by its (x, y) coordinates. The lvl indicates
 	 * the level of the desired Block. Note that if a Block includes the location
-	 * (x, y), and that Block is subdivided, then one of its sub-Blocks will 
-	 * contain the location (x, y) too. This is why we need lvl to identify 
-	 * which Block should be returned. 
-	 * 
-	 * Input validation: 
+	 * (x, y), and that Block is subdivided, then one of its sub-Blocks will
+	 * contain the location (x, y) too. This is why we need lvl to identify
+	 * which Block should be returned.
+	 *
+	 * Input validation:
 	 * - this.level <= lvl <= maxDepth (if not throw exception)
 	 * - if (x,y) is not within this Block, return null.
 	 */
@@ -215,12 +216,12 @@ public class Block {
 	}
 
 	/*
-	 * Swaps the child Blocks of this Block. 
-	 * If input is 1, swap vertically. If 0, swap horizontally. 
-	 * If this Block has no children, do nothing. The swap 
+	 * Swaps the child Blocks of this Block.
+	 * If input is 1, swap vertically. If 0, swap horizontally.
+	 * If this Block has no children, do nothing. The swap
 	 * should be propagated, effectively implementing a reflection
 	 * over the x-axis or over the y-axis.
-	 * 
+	 *
 	 */
 	public void reflect(int direction) {
 		if (!(direction == 0 || direction == 1)) {
@@ -270,8 +271,8 @@ public class Block {
 	}
 
 	/*
-	 * Rotate this Block and all its descendants. 
-	 * If the input is 1, rotate clockwise. If 0, rotate 
+	 * Rotate this Block and all its descendants.
+	 * If the input is 1, rotate clockwise. If 0, rotate
 	 * counterclockwise. If this Block has no children, do nothing.
 	 */
 	public void rotate(int direction) {
@@ -300,22 +301,22 @@ public class Block {
 			this.updateSizeAndPosition(this.size, this.xCoord, this.yCoord);
 		}
 	}
- 
+
 
 
 	/*
 	 * Smash this Block.
-	 * 
+	 *
 	 * If this Block can be smashed,
-	 * randomly generate four new children Blocks for it.  
+	 * randomly generate four new children Blocks for it.
 	 * (If it already had children Blocks, discard them.)
 	 * Ensure that the invariants of the Blocks remain satisfied.
-	 * 
-	 * A Block can be smashed iff it is not the top-level Block 
+	 *
+	 * A Block can be smashed iff it is not the top-level Block
 	 * and it is not already at the level of the maximum depth.
-	 * 
+	 *
 	 * Return True if this Block was smashed and False otherwise.
-	 * 
+	 *
 	 */
 	public boolean smash() {
 		if(this.level == 0 || this.level == this.maxDepth){
@@ -330,22 +331,24 @@ public class Block {
 			return true;
 		}
 	}
- 
- 
+
+
 	/*
 	 * Return a two-dimensional array representing this Block as rows and columns of unit cells.
-	 * 
-	 * Return and array arr where, arr[i] represents the unit cells in row i, 
+	 *
+	 * Return and array arr where, arr[i] represents the unit cells in row i,
 	 * arr[i][j] is the color of unit cell in row i and column j.
-	 * 
+	 *
 	 * arr[0][0] is the color of the unit cell in the upper left corner of this Block.
 	 */
 
 	//Unit cell = smallest size allowed by depth
 	public Color[][] flatten() {
+		Block topBlock = this.getSelectedBlock(0,0,0);
+		topBlock.updateSizeAndPosition(topBlock.size, topBlock.xCoord, topBlock.yCoord);
+
 		int unitSize = this.findUnit();
 		Color[][] returnArray = new Color[this.size/unitSize][this.size/unitSize];
-
 		this.writeArray(returnArray,unitSize);
 
 		return returnArray;
